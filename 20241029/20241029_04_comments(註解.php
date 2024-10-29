c87218bfa51e02535851b7dfa8ec4174f686132c
maplesift <a23050716@gmail.com>
2024-10-29 10:33:58 +0800

    04 Comments註解 05Text Formatting字形編排

---
+++ 20241029/20241029_04_comments(註解.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>inline_css</title>
+    <link rel="icon" href="../test.ico" sizes="32x32" type="image/png">
+    <link rel="icon" href="../test.ico" sizes="16x16" type="image/png">
+
+</head>
+
+<body>
+
+    <!-- html -->
+    <!-- ctrl + / -->
+
+    <?php 
+        // $data = "hello";
+    ?>
+
+</body>
+
+</html>
