50e12501fb8717802187232669519c64aa8b4b88
maplesift <a23050716@gmail.com>
2025-01-23 16:10:39 +0800

    CURL 練習

---
+++ 20250123/php/curl.php
@@ -0,0 +1,40 @@
+<?php
+function dd($data)
+{
+    echo "<pre>";
+    print_r($data);
+    echo "</pre>";
+}
+
+// fix cors
+header('Access-Control-Allow-Origin: *');
+
+// curl
+//init curl
+$ch = curl_init();
+
+$url= 'https://cloud.hakka.gov.tw/Pub/Opendata/DTST20230500068.json';
+
+//curl_setopt可以設定curl參數
+//設定url
+curl_setopt($ch , CURLOPT_URL , $url);
+// false 顯示
+// true 不顯示
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($ch);
+
+echo $result;
+
+// $result = curl_exec($ch);
+//關閉連線
+curl_close($ch);
+
+// dd($result);
+
+// $data = json_decode($result);
+// dd($data);
+
+// json_decode json to array
+// json_encode array to json
+
+// $url= 'https://cloud.hakka.gov.tw/Pub/Opendata/DTST20230500068.json';
